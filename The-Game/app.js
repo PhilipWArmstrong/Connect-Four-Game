2fa5d9444fb12b716050d575b46f0ffa0c130ebe
Philip Armstrong <philiparmstrong@Philips-MacBook-Pro.local>
2019-10-21 22:06:54 -0500

    start js

--- The-Game/app.js
+++ The-Game/app.js
@@ -1,3 +1,22 @@
 $(() => {
     console.log('this works');
+
+    const $shape = $('.row div')
+
+    const shapeArr = ['empty','x', 'o'];
+
+    let counter = 0;
+    const changeShape = (event) => {
+        if (counter <= shapeArr.length - 1) {
+            $(event.currentTarget).attr('class', shapeArr[counter]);
+                counter++;
+        } else {
+            counter = 0;
+            $(event.currentTarget).attr('class', shapeArr[counter]);
+            counter++;
+        }
+    };
+
+    $shape.on('click', changeShape); 
+
 });
