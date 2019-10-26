8aa5b65248dc1e9a4f917684c5833134e1891561
Philip Armstrong <philiparmstrong@Philips-MacBook-Pro.local>
2019-10-26 08:42:10 -0500

    game finished

--- The-Game/app.js
+++ The-Game/app.js
@@ -1,77 +1,145 @@
-$(() => {
-    console.log('this works');
 
-    const $shape = $('.row div')
+// 1. Create players 
+let red = 0;
+let yellow = 0;
+const start = () => {
+    // alert ('Lets make some money');
+    winner();
+}
+//2. Start keeping track of winners 
+const winner = () => {
+    const choice = prompt('Who won the game?', 'Red/Yellow');
+    if (choice === 'Red'){
+        keepRed();
+    } else if (choice === 'Yellow'){
+        keepYellow();
+    } else {
+        areYouSure();
+    }
+};
+const keepRed = () => {
+    red++;
+    alert ('Red has won' + red +' times.');
+    if (red === 2){
+        redChamp();
+    } else if (red !== 2){
+        alert('Keep playing');
+    }
+}
+const keepYellow = () => {
+    yellow++;
+    alert ('Yellow has won' + yellow +' times.');
+    if (yellow === 2){
+        yellowChamp();
+    } else if (yellow !== 2){
+        alert('Keep playing');
+    }
+}
+const areYouSure = () => {
+    alert ('It was a tie. That rounf did not count');
+    start();
+}
+const redChamp = () => {
+    alert ('Red player is the Champion. Yellow is the loser.');
+}
+const yellowChamp = () => {
+    alert ('Yellow player is the Champion. Red is the loser.');
+}
+
+////////////////////////////////////////////////////////////////
+// JQuery Starts 
+
+$(() => {
+
+
+// hold the list of winners and resets the board 
+const list = [];
+
+$('form').on('submit', (event) => {
+
+    event.preventDefault();
+    $(event.currentTarget).trigger('reset');
+
+
+    back();
+
+    start();
+});
+
+// makes sure all circles can default back to start state so a new game can begin 
+const back = () => {
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    $('#yellowPlayer').attr('id', 'square');
+    $('#redPlayer').attr('id', 'square');
+    // $('.empty2 div').attr('id', 'redPlayer');
+};
+/////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// I know this works up to a certain point. Keep in case you need to start over.
+
+// Creates the ability to click the circle and change color 
+const $shape = $('.row div')
 
     const shapeArr = ['redPlayer','yellowPlayer'];
+    const shapeArr2 = ['yellowPlayer', 'redPlayer'];
 
     let counter = 0;
-
-
-
-    // Empty Player
-    const changeShapeEmpty = (event) => {
-        $(event.currentTarget).attr('id', 'empty');
+    const changeShape = (event) => {
+        if (counter % 2 == 0) {
+            $(event.currentTarget).attr('id', shapeArr[counter]);
+            $('#redPlayer').attr('id', shapeArr2[counter]);
+            counter++;
+        } else {
+            $(event.currentTarget).attr('id', shapeArr[counter]);
+            counter = 1;
+            $('#yellowPlayer').attr('id', shapeArr2[counter]);
+            counter = 0;
+        }
     };
-    $shape.on('click', changeShapeEmpty);
-
-    // Red Player
-    const changeShapeRed = (event) => {
-        $(event.currentTarget).attr('id', 'redPlayer');
-    };
-    $shape.on('click', changeShapeRed); 
-
-    // Yellow Player
-    const changeShapeYellow = (event) => {
-        $(event.currentTarget).attr('id', 'yellowPlayer');
-    };
-    $shape.on('click', changeShapeYellow); 
-
     
 
-    // start my if statement to incorporate players 
-    
-    // const start = (event) => {
-    //     if (let something = changeShapeEmpty)
-    // }
-
-
-
-
-    // I think I want to create an if / else if that takes in .on's of red, yellow, and empty 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    ///////////////////////////////////////////////
-// I know this works up to a certain point. Keep in case you need to start over.
-// const $shape = $('.row div')
-
-//     const shapeArr = ['redPlayer','yellowPlayer'];
-
-//     let counter = 0;
-//     const changeShape = (event) => {
-//         if (counter <= shapeArr.length - 1) {
-//             $(event.currentTarget).attr('id', shapeArr[counter]);
-//                 counter++;
-//         } else {
-//             counter = 0;
-//             $(event.currentTarget).attr('id', shapeArr[counter]);
-//             counter++;
-//         }
-//     };
-
-//     $shape.on('click', changeShape); 
+    $shape.on('click', changeShape); 
     /////////////////////////////////////////////////
 
    
